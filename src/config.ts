5f31b2c7913b615b54345e20b5d9b713029aea3a
Posandu Mapa <posandumapa@gmail.com>
2023-01-22 16:34:46 +0530

    update version

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ const configBackend = {
     fileLoc: homedir() + '/terminai.json',
     cacheLoc: homedir() + '/terminai-cache.json',
     api: "https://cdn.jsdelivr.net/npm/terminai/package.json",
-    version: "1.0.7",
+    version: "1.0.8",
     prod: process.env.NODE_ENV === 'production',
 }
 
